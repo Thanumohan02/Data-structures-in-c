f6dc9626619f3cf25123c11476a44e8dd28f2384
Thanumohan02 <thanumohan29@gmail.com>
2024-09-13 09:04:47 +0530

    Add files via upload

---
+++ infiniterecursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void test()
+{
+  int n;
+  printf("Enter the valuefor n: ");
+  scanf("%d",&n);
+  if(n%2==0)
+    printf("even",n);
+  else
+    printf("odd",n);
+}
+int main(){
+  test();
+}
+  
